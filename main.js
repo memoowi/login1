8aac822c8a7e2abd23647ba024dd83029d6f4d47
memoowi <megatenlike@gmail.com>
2023-11-11 10:05:33 +0700

    update

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 //Tombol Masuk or regist #masih_cacat
 let emailInput = document.querySelector(".email");
-let passwordInput = document.querySelector(".password");
+let passwordInput = document.querySelector(".password"); 
 let inputButton = document.querySelector(".button");
 
 inputButton.disabled = true;
 
-emailInput.addEventListener("input", success);
-passwordInput.addEventListener("input", success);
+emailInput.addEventListener("input", success); 
+passwordInput.addEventListener("input", success); 
 
 function success() {
-    let emailValue = emailInput.value.trim();
-    let passwordValue = passwordInput.value.trim();
     if (emailInput.value.length > 0 && passwordInput.value.length > 0) {
         inputButton.disabled = false;
     }
@@ -25,7 +23,8 @@ const password = document.querySelector(".password");
 
 togglePassword.addEventListener("click", function () {
     // toggle type attribute
-    const type = password.getAttribute("type") === "password" ? "text" : "password"; password.setAttribute("type", type);
+    const type = password.getAttribute("type") === "password" ? "text" : "password"; 
+    password.setAttribute("type", type);
        
     // toggle icon
     this.classList.toggle("eye-slash");
